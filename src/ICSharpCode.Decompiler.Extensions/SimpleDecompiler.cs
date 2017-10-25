5b52c9bf2ec1e2961b3fc69aa3b70544b7c9089f
Christoph Wille <christoph.wille@gmail.com>
2017-10-25 10:39:00 +0200

    Add "not found" handling option

--- src/ICSharpCode.Decompiler.Extensions/SimpleDecompiler.cs
+++ src/ICSharpCode.Decompiler.Extensions/SimpleDecompiler.cs
@@ -11,74 +11,87 @@ using Mono.Cecil;
 
 namespace ICSharpCode.Decompiler.Extensions
 {
-	public class SimpleDecompiler
-	{
-		private readonly ModuleDefinition _module;
-		private readonly DecompilerTypeSystem _typeSystem;
+    /// <summary>
+    /// SimpleDecompiler only supports CSharpDecompiler
+    /// </summary>
+    public class SimpleDecompiler
+    {
+        private readonly ModuleDefinition _module;
+        private readonly DecompilerTypeSystem _typeSystem;
 
-		private SimpleDecompiler()
-		{
-		}
-		private SimpleDecompiler(ModuleDefinition module, DecompilerTypeSystem typeSystem)
-		{
-			_module = module;
-			_typeSystem = typeSystem;
-		}
+        private SimpleDecompiler()
+        {
+        }
+        private SimpleDecompiler(ModuleDefinition module, DecompilerTypeSystem typeSystem)
+        {
+            _module = module;
+            _typeSystem = typeSystem;
+        }
 
-		public ModuleDefinition ModuleDefinition => _module;
-		public DecompilerTypeSystem TypeSystem => _typeSystem;
+        public ModuleDefinition ModuleDefinition => _module;
+        public DecompilerTypeSystem TypeSystem => _typeSystem;
 
-		public static SimpleDecompiler Create(ModuleDefinition module)
-		{
-			var typeSystem = new DecompilerTypeSystem(module);
-			return new SimpleDecompiler(module, typeSystem);
-		}
+        public static SimpleDecompiler Create(ModuleDefinition module)
+        {
+            var typeSystem = new DecompilerTypeSystem(module);
+            return new SimpleDecompiler(module, typeSystem);
+        }
 
-		public void ListContent(TextWriter output, ISet<TypeKind> kinds)
-		{
-			foreach (ITypeDefinition type in ListContent(kinds)) {
-				output.WriteLine($"{type.Kind} {type.FullName}");
-			}
-		}
+        public void ListContent(TextWriter output, ISet<TypeKind> kinds)
+        {
+            foreach (ITypeDefinition type in ListContent(kinds)) {
+                output.WriteLine($"{type.Kind} {type.FullName}");
+            }
+        }
 
-		public IEnumerable<ITypeDefinition> ListContent(ISet<TypeKind> kinds)
-		{
-			foreach (ITypeDefinition type in ListContent()) {
-				if (!kinds.Contains(type.Kind))
-					continue;
-				yield return type;
-			}
-		}
+        public IEnumerable<ITypeDefinition> ListContent(ISet<TypeKind> kinds)
+        {
+            foreach (ITypeDefinition type in ListContent()) {
+                if (!kinds.Contains(type.Kind))
+                    continue;
+                yield return type;
+            }
+        }
 
-		public IEnumerable<ITypeDefinition> ListContent()
-		{
-			return _typeSystem.MainAssembly.GetAllTypeDefinitions();
-		}
+        public IEnumerable<ITypeDefinition> ListContent()
+        {
+            return _typeSystem.MainAssembly.GetAllTypeDefinitions();
+        }
 
-		public void DecompileAsProject(string outputDirectory)
-		{
-			WholeProjectDecompiler decompiler = new WholeProjectDecompiler();
-			decompiler.DecompileProject(_module, outputDirectory);
-		}
+        public void DecompileAsProject(string outputDirectory)
+        {
+            WholeProjectDecompiler decompiler = new WholeProjectDecompiler();
+            decompiler.DecompileProject(_module, outputDirectory);
+        }
 
-		public CSharpDecompiler InitializeDecompiler()
-		{
-			return new CSharpDecompiler(_typeSystem, new DecompilerSettings());
-		}
+        public CSharpDecompiler InitializeDecompiler()
+        {
+            return new CSharpDecompiler(_typeSystem, new DecompilerSettings());
+        }
 
-		public void Decompile(TextWriter output, string typeName = null)
-		{
-			CSharpDecompiler decompiler = InitializeDecompiler();
+        public void Decompile(TextWriter output, string typeName = null, bool throwOnTypeNotFound = false)
+        {
+            CSharpDecompiler decompiler = InitializeDecompiler();
 
-			decompiler.AstTransforms.Add(new EscapeInvalidIdentifiers());
-			SyntaxTree syntaxTree;
-			if (typeName == null)
-				syntaxTree = decompiler.DecompileWholeModuleAsSingleFile();
-			else
-				syntaxTree = decompiler.DecompileTypes(_module.GetTypes().Where(td => string.Equals(td.FullName, typeName, StringComparison.OrdinalIgnoreCase)));
+            decompiler.AstTransforms.Add(new EscapeInvalidIdentifiers());
+            SyntaxTree syntaxTree;
 
-			var visitor = new CSharpOutputVisitor(output, FormattingOptionsFactory.CreateSharpDevelop());
-			syntaxTree.AcceptVisitor(visitor);
-		}
-	}
+            if (typeName == null) {
+                syntaxTree = decompiler.DecompileWholeModuleAsSingleFile();
+            } else {
+                var typeToDecompile = _module.GetTypes()
+                    .Where(td => string.Equals(td.FullName, typeName, StringComparison.OrdinalIgnoreCase))
+                    .ToList(); // we need multiple enumeration (Any() + DecompileTypes)
+
+                if (!typeToDecompile.Any() && throwOnTypeNotFound) {
+                    throw new ArgumentException($"Type {typeName} not found, cannot decompile");
+                }
+
+                syntaxTree = decompiler.DecompileTypes(typeToDecompile);
+            }
+
+            var visitor = new CSharpOutputVisitor(output, FormattingOptionsFactory.CreateSharpDevelop());
+            syntaxTree.AcceptVisitor(visitor);
+        }
+    }
 }
