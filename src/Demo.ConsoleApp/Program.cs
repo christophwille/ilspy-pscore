7cfb1adfcc16fc727d4aa08b004cfcd93a792385
Christoph Wille <christoph.wille@gmail.com>
2017-10-25 10:06:10 +0200

    Add a demo project for API prototyping

---
+++ src/Demo.ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using ICSharpCode.Decompiler.Extensions;
+using ICSharpCode.Decompiler.TypeSystem;
+
+namespace Demo.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var asm = SimpleAssemblyLoader.LoadModule("ICSharpCode.Decompiler.Extensions.dll");
+            var dc = SimpleDecompiler.Create(asm);
+
+            var tds = dc.ListContent(new HashSet<TypeKind>() { TypeKind.Class });
+
+            Console.WriteLine(tds.Count());
+        }
+    }
+}
